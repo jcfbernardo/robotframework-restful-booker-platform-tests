8c7af755aead89871a8f57de0800c951525d2b0b
jcfbernardo <joaocfb.dev@gmail.com>
2026-05-23 23:16:24 -0300

    Fix schema_validator: use module-level functions so RF auto-discovers keywords

--- libraries/schema_validator.py
+++ libraries/schema_validator.py
@@ -1,47 +1,44 @@
 import jsonschema
 
-
-class SchemaValidator:
-
-    ROBOT_LIBRARY_SCOPE = "GLOBAL"
-
-    BOOKING_SCHEMA = {
-        "type": "object",
-        "required": ["bookingid", "booking"],
-        "properties": {
-            "bookingid": {"type": "integer"},
-            "booking": {
-                "type": "object",
-                "required": ["firstname", "lastname", "depositpaid", "bookingdates"],
-                "properties": {
-                    "firstname": {"type": "string"},
-                    "lastname": {"type": "string"},
-                    "depositpaid": {"type": "boolean"},
-                    "bookingdates": {
-                        "type": "object",
-                        "required": ["checkin", "checkout"],
-                    },
+BOOKING_SCHEMA = {
+    "type": "object",
+    "required": ["bookingid", "booking"],
+    "properties": {
+        "bookingid": {"type": "integer"},
+        "booking": {
+            "type": "object",
+            "required": ["firstname", "lastname", "depositpaid", "bookingdates"],
+            "properties": {
+                "firstname": {"type": "string"},
+                "lastname": {"type": "string"},
+                "depositpaid": {"type": "boolean"},
+                "bookingdates": {
+                    "type": "object",
+                    "required": ["checkin", "checkout"],
                 },
             },
         },
-    }
+    },
+}
 
-    ROOM_LIST_SCHEMA = {
-        "type": "object",
-        "required": ["rooms"],
-        "properties": {
-            "rooms": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "required": ["roomid", "roomName", "type", "roomPrice"],
-                },
-            }
-        },
-    }
+ROOM_LIST_SCHEMA = {
+    "type": "object",
+    "required": ["rooms"],
+    "properties": {
+        "rooms": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "required": ["roomid", "roomName", "type", "roomPrice"],
+            },
+        }
+    },
+}
 
-    def validate_booking_response_schema(self, response_dict):
-        jsonschema.validate(instance=response_dict, schema=self.BOOKING_SCHEMA)
 
-    def validate_room_list_response_schema(self, response_dict):
-        jsonschema.validate(instance=response_dict, schema=self.ROOM_LIST_SCHEMA)
+def validate_booking_response_schema(response_dict):
+    jsonschema.validate(instance=response_dict, schema=BOOKING_SCHEMA)
+
+
+def validate_room_list_response_schema(response_dict):
+    jsonschema.validate(instance=response_dict, schema=ROOM_LIST_SCHEMA)
